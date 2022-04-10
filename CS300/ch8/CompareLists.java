5f2409d0a26720070eec1f6512642f25fd6b3db6
zachhealy <zachjhealy@gmail.com>
2022-04-10 13:56:09 -0500

    Update CompareLists.java

--- CS300/ch8/CompareLists.java
+++ CS300/ch8/CompareLists.java
@@ -1,70 +1,71 @@
+package CS300.ch8;
 /*
 * Group Members: John Stout, Zach Healy, Noah Little, Jacob Thieret,
 * Justin Kehoe, Trinity Smallwood, and Jalik Smith.
 */
 
-
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom; //ThreadLocalRandom used instead of random due to the increased efficiency
 
 class CompareLists {
-    //initialize ArrayList and HashSet
+    // initialize ArrayList and HashSet
     static ArrayList<Double> MyArrayList = new ArrayList<Double>();
     static HashSet<Double> MyHashSet = new HashSet<Double>();
-    //make extra list for testing
+    // make extra list for testing
     static ArrayList<Double> testValuesList = new ArrayList<Double>();
 
     public static void main(String args[]) {
         Random random = new Random();
-        //initialize value to add to arraylist/hashset
+        // initialize value to add to arraylist/hashset
         double doubleValue;
 
-        //add random values from 0-999999 to both MyArrayList, as well as MyHashSet
+        // add random values from 0-999999 to both MyArrayList, as well as MyHashSet
         for (int i = 0; i < 1000000; i++) {
             doubleValue = ThreadLocalRandom.current().nextDouble(0, 999999);
             MyArrayList.add(doubleValue);
             MyHashSet.add(doubleValue);
         }
 
-        //add random values from 0-999999 to testValuesList for comparing
+        // add random values from 0-999999 to testValuesList for comparing
         for (int i = 0; i < 1000000; i++) {
             doubleValue = ThreadLocalRandom.current().nextDouble(0, 1999999);
             testValuesList.add(doubleValue);
         }
 
-        //run methods to test the time for ArrayList and HashSet
+        // run methods to test the time for ArrayList and HashSet
         testTimeForArrayList();
         testTimeForHashSet();
     }
-    
+
     static void testTimeForArrayList() {
         System.out.println("ArrayList Test Starting...");
-        long startTime = System.currentTimeMillis(); //initialize and set startTime to current system time in milliseconds
-        for (Double value : testValuesList) { //test if each value in testValuesList is in MyArrayList
+        long startTime = System.currentTimeMillis(); // initialize and set startTime to current system time in
+                                                     // milliseconds
+        for (Double value : testValuesList) { // test if each value in testValuesList is in MyArrayList
             for (Double listValue : MyArrayList) {
                 if (value == listValue)
                     break;
             }
         }
-        long endTime = System.currentTimeMillis(); //initialize and set endTime to current system time in milliseconds
+        long endTime = System.currentTimeMillis(); // initialize and set endTime to current system time in milliseconds
 
         System.out.print("Test time for MyArrayList :");
-        System.out.println((endTime - startTime) + " milliseconds"); //calculate and print runtime
+        System.out.println((endTime - startTime) + " milliseconds"); // calculate and print runtime
     }
 
-    
     static void testTimeForHashSet() {
         System.out.println("HashSet Test Starting...");
-        long startTime = System.currentTimeMillis(); //initialize and set startTime to current system time in milliseconds
-        for (Double value : testValuesList) { //test if each value in testValuesList is in MyArrayList
+        long startTime = System.currentTimeMillis(); // initialize and set startTime to current system time in
+                                                     // milliseconds
+        for (Double value : testValuesList) { // test if each value in testValuesList is in MyArrayList
             for (Double setValue : MyHashSet) {
                 if (value == setValue)
                     break;
             }
         }
-        long endTime = System.currentTimeMillis(); //initialize and set endTime to current system time in milliseconds
+        long endTime = System.currentTimeMillis(); // initialize and set endTime to current system time in milliseconds
 
         System.out.print("Test time for MyHashSet: ");
-        System.out.println((endTime - startTime) + " milliseconds"); //calculate and print runtime
+        System.out.println((endTime - startTime) + " milliseconds"); // calculate and print runtime
     }
 }
